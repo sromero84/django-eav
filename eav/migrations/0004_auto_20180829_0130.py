5da654d6162984afa99bd9c042c6b6043449615a
Santiago Romero <s.romero@mozio.com>
2018-08-28 15:31:23 -0300

    Fix migration

--- eav/migrations/0004_auto_20180829_0130.py
+++ eav/migrations/0004_auto_20180829_0130.py
@@ -6,7 +6,6 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('contenttypes', '0002_remove_content_type_name'),
         ('eav', '0003_auto_20180825_0342'),
     ]
 
